68895e22b4528c9d181351e3198c194202a5a730
joletty <happytk70@gmail.com>
2024-10-28 09:33:18 +0700

    Update historical.py

--- historical.py
+++ historical.py
@@ -17,7 +17,7 @@ def main():
     df_v = pd.read_csv('2023_vwap.csv')
     
     # Create a chart
-    chart = StreamlitChart(width=800, height=800)
+    chart = StreamlitChart(width=300, height=500)
 
     chart.legend(visible=True)
 
